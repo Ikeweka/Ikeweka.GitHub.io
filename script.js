3f52b8ebf1280a405e4417b76c4d0251f519a852
Ikeweka <ikponmwonsa.eweka@gmail.com>
2017-03-05 20:32:28 -0500

    First Website Upload

---
+++ script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<title>IKAY</title>
+<script type="text/javascript">
+function submitform()
+//loading form to submit
+    document.forms["contact"].submit();
+}
+</script>
+<form id="contact details" action="submit-form.php">
+Search: <input type='text' name='js'>// search the input
+<a href="js/script.js: submitform()">Submit</a>// connecting to the link
+</form>
+
